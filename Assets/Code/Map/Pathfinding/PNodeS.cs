9fb2b08aa1323dfb3a33fd1137fc32576ff1897d
Ash Dev <69819379+ash-dvlpr@users.noreply.github.com>
2021-03-23 23:17:50 +0100

    Reimplementando el pathfinding
    Reimplementando el pathfinding con structs y un array temporal.
    
    Además. había movido un poco las cosas

---
+++ Assets/Code/Map/Pathfinding/PNodeS.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PNodeS {
+	//? Data
+	public readonly Vector2Int position;
+	public PNodeS? previous;
+
+	public readonly bool isWalkable;
+    public readonly float pathCost;
+
+	public int gCost;
+	public int hCost;
+	public int fCost { get => gCost + gCost; }
+	
+	//? Constructors
+	public PNodeS(Vector2Int position, bool isWalkable, float pathCost, int gCost, int hCost, PNodeS? previous = null) {
+		this.position = position;
+		this.previous = previous;
+
+		this.isWalkable = isWalkable;
+		this.pathCost = pathCost;
+
+		this.gCost = gCost;
+		this.hCost = hCost;
+    }
+
+	//? Methods
+	
+}
