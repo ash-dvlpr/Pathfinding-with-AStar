ea21fa7aa7eebae62ae3ef39ee4d93b1aff5183f
Ash Dev <69819379+ash-dvlpr@users.noreply.github.com>
2021-03-24 00:34:15 +0100

    Restaurando las pérdidas

--- Assets/Code/Map/Pathfinding/PNodeS.cs
+++ Assets/Code/Map/Pathfinding/PNodeS.cs
@@ -2,17 +2,5 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PNodeS : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public struct PNodeS {
 }
